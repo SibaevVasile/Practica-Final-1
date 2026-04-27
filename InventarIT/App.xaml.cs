cd7c4a085b4e338f0aa66824a6d103ebcf4ca662
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-04-27 14:04:47 +0300

    feat: add DatabaseHelper with connection string management

--- InventarIT/App.xaml.cs
+++ InventarIT/App.xaml.cs
@@ -5,13 +5,26 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using InventarIT.Data;
 
 namespace InventarIT
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var db = new DatabaseHelper();
+            if (!db.TesteazaConexiunea())
+            {
+                MessageBox.Show(
+                    "Nu s-a putut conecta la baza de date!\n" +
+                    "Verificați că SQL Server Express rulează.",
+                    "Eroare conexiune",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+            }
+        }
     }
 }
---
+++ InventarIT/Data/DatabaseHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace InventarIT.Data
+{
+    public class DatabaseHelper
+    {
+        private readonly string _connectionString;
+
+        public DatabaseHelper()
+        {
+            _connectionString = GetConnectionString();
+        }
+
+        private static string GetConnectionString()
+        {
+            return "Server=localhost\\SQLEXPRESS;" +
+                   "Database=InventarITDB;" +
+                   "Trusted_Connection=True;" +
+                   "TrustServerCertificate=True;";
+        }
+
+        public SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+
+        public bool TesteazaConexiunea()
+        {
+            try
+            {
+                using var connection = GetConnection();
+                connection.Open();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
