b2f91a5313c47f1f76db98e6398d6301791ecf62
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-05-06 15:29:44 +0300

    feat: bind GetAllEchipamente data to DataGrid on page load #13

--- InventarIT/Views/EchipamentView.xaml.cs
+++ InventarIT/Views/EchipamentView.xaml.cs
@@ -1,28 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
+using InventarIT.Data;
 
 namespace InventarIT.Views
 {
-    /// <summary>
-    /// Interaction logic for Page1.xaml
-    /// </summary>
-    public partial class Page1 : Page
+    public partial class EchipamentView : Page
     {
-        public Page1()
+        private readonly DatabaseHelper _db = new();
+
+        public EchipamentView()
         {
             InitializeComponent();
+            IncarcaDate();
+        }
+
+        private void IncarcaDate()
+        {
+            dgEchipamente.ItemsSource = _db.GetAllEchipamente();
         }
     }
 }
