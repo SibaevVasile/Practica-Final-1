cffcc38eb96361e63a9f40810361d8ed136471e3
SibaevVasile <157913814+SibaevVasile@users.noreply.github.com>
2026-05-11 17:26:04 +0300

    feat: load angajati data from DB on AngajatView page load #14

--- InventarIT/Views/AngajatView.xaml.cs
+++ InventarIT/Views/AngajatView.xaml.cs
@@ -1,28 +1,60 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using InventarIT.Data;
+using InventarIT.Models;
 
 namespace InventarIT.Views
 {
-    /// <summary>
-    /// Interaction logic for AngajatView.xaml
-    /// </summary>
     public partial class AngajatView : Page
     {
+        private readonly DatabaseHelper _db = new();
+        private List<Angajat> _totiAngajatii = new();
+
         public AngajatView()
         {
             InitializeComponent();
+            IncarcaDate();
+        }
+
+        private void IncarcaDate()
+        {
+            _totiAngajatii = _db.GetAllAngajati();
+            dgAngajati.ItemsSource = _totiAngajatii;
+        }
+
+        private void txtCautare_TextChanged(
+            object sender, TextChangedEventArgs e)
+        {
+            var termen = txtCautare.Text.ToLower().Trim();
+            if (string.IsNullOrEmpty(termen))
+            {
+                dgAngajati.ItemsSource = _totiAngajatii;
+            }
+            else
+            {
+                dgAngajati.ItemsSource = _totiAngajatii
+                    .Where(a =>
+                        a.Nume.ToLower().Contains(termen) ||
+                        a.Email.ToLower().Contains(termen))
+                    .ToList();
+            }
+        }
+
+        private void btnAdauga_Click(object sender, RoutedEventArgs e)
+        {
+            // Implementat la Issue #15
+        }
+
+        private void btnModifica_Click(object sender, RoutedEventArgs e)
+        {
+            // Implementat la Issue #15
+        }
+
+        private void btnSterge_Click(object sender, RoutedEventArgs e)
+        {
+            // Implementat la Issue #16
         }
     }
 }
